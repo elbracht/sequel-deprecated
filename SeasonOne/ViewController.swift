7609ec011e02844c195ae5344d1a64c40480b50c
Alexander Elbracht <alexander@elbracht.org>
2017-09-28 00:30:27 +0200

    Initial commit

---
+++ SeasonOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeasonOne
+//
+//  Created by Alexander Elbracht on 28.09.17.
+//  Copyright © 2017 Alexander Elbracht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
