10e4ad671e03f3adf60df8aae6a22593f231ed57
Alexander Elbracht <alexander@elbracht.org>
2017-09-28 00:32:23 +0200

    Initialize CocoaPods

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '11.0'
+
+target 'SeasonOne' do
+  use_frameworks!
+end
---
+++ Pods/Target Support Files/Pods-SeasonOne/Pods-SeasonOne-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeasonOne : NSObject
+@end
+@implementation PodsDummy_Pods_SeasonOne
+@end
