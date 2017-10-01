30afe6b5783deacf9dc95caed8b61f141a9bdc2c
Alexander Elbracht <alexander@elbracht.org>
2017-10-01 14:31:06 +0200

    ✅ Extend tests for IconTextField

--- SeasonOneTests/IconTextFieldTests.swift
+++ SeasonOneTests/IconTextFieldTests.swift
@@ -21,11 +21,15 @@ class IconTextFieldTests: XCTestCase {
     
     func testInit() {
         let iconTextField = IconTextField(frame: CGRect())
+        iconTextField.padding = UIEdgeInsets(top: 0, left: 32, bottom: 0, right: 32)
+        iconTextField.font = UIFont(name: "Arial", size: 15)
         iconTextField.icon = UIImage(named: "search")
         iconTextField.iconColor = UIColor.black
+        iconTextField.iconPadding = 16
         iconTextField.placeholder = "Search"
         iconTextField.placeholderColor = UIColor.black
         iconTextField.clearButtonImage = UIImage(named: "clear")
+        iconTextField.clearButtonPadding = 16
         
         XCTAssertNotNil(iconTextField)
     }
