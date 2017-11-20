50f4c17129cc3e1f7c8f44d830410f5e282ed371
Alexander Elbracht <alexander@elbracht.org>
2017-11-20 17:40:08 +0100

    Add blur header view behind SearchTextField

--- Sequel/Views/Search/SearchView.swift
+++ Sequel/Views/Search/SearchView.swift
@@ -3,8 +3,9 @@ import SwiftTheme
 import UIKit
 
 struct SearchViewMeasure {
+    static let searchHeaderHeight = 64 as CGFloat
     static let searchInputOffsetTop = 12 as CGFloat
-    static let searchCollectionViewOffset = UIEdgeInsets(top: 72.0, left: 16.0, bottom: 16.0, right: 16.0)
+    static let searchCollectionViewOffset = UIEdgeInsets(top: 76.0, left: 16.0, bottom: 16.0, right: 16.0)
     static let searchItemOffset = 12 as CGFloat
     static let searchItemPerRow = 2 as CGFloat
     static let searchItemRatioWidth = 2 as CGFloat
@@ -12,6 +13,7 @@ struct SearchViewMeasure {
 }
 
 class SearchView: UIView {
+    public var searchHeaderView: UIVisualEffectView!
     public var searchInputView: SearchInputView!
     public var searchCollectionView: UICollectionView!
 
@@ -20,6 +22,7 @@ class SearchView: UIView {
 
         initView()
         initSearchCollectionView()
+        initSearchHeader()
         initSearchInputView()
     }
 
@@ -33,19 +36,25 @@ class SearchView: UIView {
         UIApplication.shared.theme_setStatusBarStyle(statusBarStylePicker, animated: true)
     }
 
+    private func initSearchHeader() {
+        searchHeaderView = UIVisualEffectView(effect: UIBlurEffect(style: .extraLight))
+        self.addSubview(searchHeaderView)
+
+        searchHeaderView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self)
+            make.left.equalTo(self)
+            make.right.equalTo(self)
+            make.height.equalTo(UIApplication.shared.statusBarFrame.height + SearchViewMeasure.searchHeaderHeight)
+        }
+    }
+
     private func initSearchInputView() {
         searchInputView = SearchInputView()
         searchInputView.showCancelButton()
         self.addSubview(searchInputView)
 
         searchInputView.snp.makeConstraints { (make) in
-            if #available(iOS 11, *) {
-                make.top.equalTo(self.safeAreaLayoutGuide.snp.topMargin).offset(SearchViewMeasure.searchInputOffsetTop)
-            } else {
-                let statusBarHeight = UIApplication.shared.statusBarFrame.height
-                make.top.equalTo(self).offset(statusBarHeight + SearchViewMeasure.searchInputOffsetTop)
-            }
-
+            make.top.equalTo(self).offset(UIApplication.shared.statusBarFrame.height + SearchViewMeasure.searchInputOffsetTop)
             make.left.equalTo(self)
             make.right.equalTo(self)
             make.height.equalTo(SearchInputViewMeasure.height)
