333302de8a68ce0879ec9a11eaea0acdeac941aa
Alexander Elbracht <alexander@elbracht.org>
2018-08-29 15:58:06 +0200

    🛠 Rename short variable names to fix linter error

--- Sequel/Helper/UIImage+Darkness.swift
+++ Sequel/Helper/UIImage+Darkness.swift
@@ -8,11 +8,11 @@ extension UIImage {
             let length = CFDataGetLength(imageData)
             let threshold = Int(Double(image.width * image.height) * 0.45)
             var darkPixels = 0
-            for i in stride(from: 0, to: length, by: 4) {
-                let r = ptr[i]
-                let g = ptr[i + 1]
-                let b = ptr[i + 2]
-                let luminance = (0.299 * Double(r) + 0.587 * Double(g) + 0.114 * Double(b))
+            for byte in stride(from: 0, to: length, by: 4) {
+                let red = ptr[byte]
+                let green = ptr[byte + 1]
+                let blue = ptr[byte + 2]
+                let luminance = (0.299 * Double(red) + 0.587 * Double(green) + 0.114 * Double(blue))
                 if luminance < 150 {
                     darkPixels += 1
                     if darkPixels > threshold {
